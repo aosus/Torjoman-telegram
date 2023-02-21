0a84534815379c7afce77b51dc2849901d89acd4
Karam Al-Hamada <krmhmade1@gmail.com>
2023-02-21 11:13:47 +0300

    pref: remove unnecessary calls

--- plugins/settings/main.py
+++ plugins/settings/main.py
@@ -6,10 +6,6 @@ from pytorjoman import Account, errors
 
 @Client.on_message(filters.command(['settings']) & myfilters.user_is_registered)
 async def main_menu(c, msg: Message):
-    user = await User.objects.get_or_none(
-        user_id=msg.from_user.id
-    )
-    u = await Account.login_from_token(c.torjoman, user.access_token)
     await msg.reply(texts.SETTINGS_MAIN, reply_markup=InlineKeyboardMarkup(
         [
             [InlineKeyboardButton(texts.CHANGE_PREFERENCES, "settings_change_preferences")],
