05f965031e91ffc8a304d8030b12e7f020e5cef6
Karam Al-Hamada <krmhmade1@gmail.com>
2023-02-21 12:04:52 +0300

    feat: add change preferences main menu

---
+++ plugins/settings/change_preferences/main.py
@@ -0,0 +1,22 @@
+from pyrogram import Client , filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from db import User
+from plugins.start import start
+from utils import texts, filters as myfilters
+from pytorjoman import Account, errors
+
+
+@Client.on_callback_query(filters.regex('^settings_change_preferences$'))
+async def change_preferences_main(_, cb: CallbackQuery):
+    user = await User.objects.get(user_id=cb.from_user.id)
+    user.data['step'] = "main"
+    await user.update(['data'])
+    await cb.edit_message_reply_markup(InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton(texts.FIRST_NAME, "settings_change_first_name")],
+            [InlineKeyboardButton(texts.LAST_NAME, "settings_change_last_name")],
+            [InlineKeyboardButton(texts.SENDING_TIME, "settings_change_sending_time")],
+            [InlineKeyboardButton(texts.NUMBER_OF_WORDS, "settings_change_number_of_words")],
+        ]
+    ))
+
